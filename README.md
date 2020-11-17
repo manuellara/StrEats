d6089bb376728e04fa9bf81deb75dcbc8573c397
Manuel Lara <manuellaraa@gmail.com>
2020-11-16 19:09:26 -0800

    updated readme and layout

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-# StrEats
+# StrEats - Mobile Ordering Service
 
 Prototype website for mobile ordering service
+
+## Built with...
+
+React 
+Material-UI
--- src/Layout.js
+++ src/Layout.js
@@ -4,7 +4,7 @@ import React from 'react'
 export default function Layout() {
     return (
         <div>
-            Hello World
+            <h1>Hello World</h1>
         </div>
     )
 }
