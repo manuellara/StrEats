2833adb04bfb0ee42b60982bc03b9f850d9da195
Manuel Lara <manuellaraa@gmail.com>
2020-11-18 02:06:03 -0800

    added firebase and a components folder

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
 // components 
-import Layout from './Layout';
+import Layout from './components/Layout';
+import Signup from './components/Signup';
+
 
 function App() {
   return (
     <div>
       <Layout />
+      <Signup />
     </div>
   );
 }
---
+++ src/components/Signup.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Card, CardContent } from "@material-ui/core";
+
+
+
+const useStyles = makeStyles({
+  root: null,
+});
+
+export default function Signup() {
+  const classes = useStyles();
+
+  return (
+    <>
+      <Card className={classes.root}>
+          <CardContent>
+              <h2>Sign Up</h2>
+          </CardContent>
+      </Card>
+
+      <div>
+          Already have an account? Login
+      </div>
+    </>
+  );
+}
