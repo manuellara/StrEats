1c32e22de2a0aa1b0c152188e80c3a9a8b943eba
manuel lara <manuellaraa@gmail.com>
2020-11-22 17:58:31 -0800

    added Formik and Context

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import Container from "@material-ui/core/Container";
+import { AuthProvider } from "./contexts/AuthContext";
 
-// components 
-import Signup from './components/Signup';
-
+// components
+import Signup from "./components/Signup";
 
 function App() {
   return (
-    <div>
-      <Signup />
-    </div>
+    <AuthProvider>
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justify="center"
+        style={{ minHeight: "100vh" }}
+      >
+        <Container maxWidth="sm">
+          <Signup />
+        </Container>
+      </Grid>
+    </AuthProvider>
   );
 }
 
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -2,9 +2,15 @@ import React from "react";
 import { Formik, Form, Field } from "formik";
 import { Button, LinearProgress } from "@material-ui/core";
 import { TextField } from "formik-material-ui";
-import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
+import Card from "@material-ui/core/Card";
+import Grid from "@material-ui/core/Grid";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
 import MuiTextField from "@material-ui/core/TextField";
 import { fieldToTextField } from "formik-material-ui";
+
+import { useAuth } from "../contexts/AuthContext";
 
 function UpperCasingTextField(props) {
   const {
@@ -22,67 +28,103 @@ function UpperCasingTextField(props) {
 }
 
 export default function Signup() {
+  const { signUp } = useAuth();
+
   return (
-    <Formik
-      initialValues={{
-        email: "",
-        password: "",
-      }}
-      validate={(values) => {
-        const errors = {};
-        if (!values.email) {
-          errors.email = "Required";
-        } else if (
-          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
-        ) {
-          errors.email = "Invalid email address";
-        }
+    <>
+      <Formik
+        initialValues={{
+          email: "",
+          password: "",
+          passwordConfirm: "",
+        }}
+        validate={(values) => {
+          const errors = {};
+          if (!values.email) {
+            errors.email = "Required";
+          } else if (
+            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+          ) {
+            errors.email = "Invalid email address";
+          }
 
-        if (values.password < 8) {
-          errors.password = "Password needs to be at lest 8 characters";
-        }
+          if (values.password.length < 8) {
+            errors.password = "Password needs to be at lest 8 characters";
+          }
 
-        return errors;
-      }}
-      onSubmit={(values, { setSubmitting }) => {
-        setTimeout(() => {
-          setSubmitting(false);
-          alert(JSON.stringify(values, null, 2));
-        }, 500);
-      }}
-    >
-      {({ submitForm, isSubmitting }) => (
-        <Form>
-          {isSubmitting && <LinearProgress />}
-          <Box margin={1}>
-            <Field
-              component={UpperCasingTextField}
-              name="email"
-              type="email"
-              label="Email"
-              helperText="Please Enter Email"
-            />
-          </Box>
-          <Box margin={1}>
-            <Field
-              component={TextField}
-              type="password"
-              label="Password"
-              name="password"
-            />
-          </Box>
-          <Box margin={1}>
-            <Button
-              variant="contained"
-              color="primary"
-              disabled={isSubmitting}
-              onClick={submitForm}
-            >
-              Submit
-            </Button>
-          </Box>
-        </Form>
-      )}
-    </Formik>
+          if (values.passwordConfirm !== values.password) {
+            errors.passwordConfirm = "Passwords do not match";
+          }
+
+          return errors;
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            setSubmitting(false);
+            // alert(JSON.stringify(values, null, 2));
+            signUp(values.email, values.password);
+          }, 500);
+        }}
+      >
+        {({ submitForm, isSubmitting }) => (
+          <Form>
+            {isSubmitting && <LinearProgress />}
+            <Card variant="outlined">
+              <Typography variant="h4" align="center">
+                Sign Up
+              </Typography>
+              <CardContent>
+                <Grid container spacing={3}>
+                  <Grid item xs={12}>
+                    <Field
+                      component={UpperCasingTextField}
+                      variant="outlined"
+                      name="email"
+                      type="email"
+                      label="Email"
+                      fullWidth
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Field
+                      component={TextField}
+                      variant="outlined"
+                      type="password"
+                      label="Password"
+                      name="password"
+                      fullWidth
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Field
+                      component={TextField}
+                      variant="outlined"
+                      type="password"
+                      label="Confirm Password"
+                      name="passwordConfirm"
+                      fullWidth
+                    />
+                  </Grid>
+                </Grid>
+              </CardContent>
+              <CardActions>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  disabled={isSubmitting}
+                  onClick={submitForm}
+                  fullWidth
+                >
+                  Submit
+                </Button>
+              </CardActions>
+            </Card>
+          </Form>
+        )}
+      </Formik>
+      <Typography variant="h6" align="center">
+        Already have an account?
+      </Typography>
+    </>
   );
 }
